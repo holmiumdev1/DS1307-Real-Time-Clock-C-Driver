cc4fb11f1d5b8b8b32153fdcb203571b12106702
holmiumdev1 <158459896+holmiumdev1@users.noreply.github.com>
2024-11-27 18:05:40 +0530

    Update rtc_ds1307_low_level.c

--- rtc_ds1307_low_level.c
+++ rtc_ds1307_low_level.c
@@ -1,9 +1,7 @@
 /*ds1307 low level api - Reza Ebrahimi v1.0*/
+
+
 #include "include/rtc_ds1307.h"
-#include "freertos/FreeRTOS.h"
-#include "driver/i2c.h"
-#include "string.h"
-#include "stdint.h"
 /*function to transmit one byte of data to register_address on ds1307*/
 void time_i2c_write_single(uint8_t device_address, uint8_t register_address, uint8_t *data_byte)
 {
@@ -40,7 +38,7 @@ void time_i2c_read_single(uint8_t device_address, uint8_t register_address, uint
 /*function to read an array of data from device_address*/
 void time_i2c_read_multi(uint8_t device_address, uint8_t start_register_address, uint8_t *data_array, uint8_t data_length)
 {
-    memset(&data_length[0],0,data_length+1);
+    memset(&data_array[0],0,data_length+1);
     i2c_master_write_read_device(I2C_NUM_0, device_address,
                                        &start_register_address, 1,
                                        data_array, data_length,

aaf37718f4b21fca805bf1e7efc1dc5afbaa47d6
holmiumdev1 <158459896+holmiumdev1@users.noreply.github.com>
2024-11-27 17:29:09 +0530

    Update rtc_ds1307.c
    POINTER ISSUES RECTIFIED

--- rtc_ds1307.c
+++ rtc_ds1307.c
@@ -46,7 +46,7 @@ uint8_t DS1307_init(uint8_t *data_array, uint8_t run_state, uint8_t reset_state)
 /*we use 1 byte of ds1307 ram to preserve the initialization status. this function reads that 1 byte*/
 uint8_t DS1307_init_status_report()
 {
-  time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_INIT_STATUS, &register_current_value);
+  time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_INIT_STATUS, register_current_value);
   if (register_current_value == DS1307_INITIALIZED)
     return DS1307_INITIALIZED;
   else
@@ -57,7 +57,7 @@ uint8_t DS1307_init_status_report()
 void DS1307_init_status_update()
 {
   register_new_value = DS1307_INITIALIZED;
-  time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_INIT_STATUS, &register_new_value);
+  time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_INIT_STATUS, register_new_value);
 }
 
 /*function to start or halt the operation of DS1307, using CH control bit in SECONDS register
@@ -65,7 +65,7 @@ void DS1307_init_status_update()
 uint8_t DS1307_run(uint8_t run_state)
 {
   /*preserving the contents of SECONDS register and changing CH bit*/
-  time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+  time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
   if (run_state == CLOCK_RUN)
   {
     /*CH=0 runs the clock*/
@@ -79,14 +79,14 @@ uint8_t DS1307_run(uint8_t run_state)
   else
     return OPERATION_FAILED;
   /*write the new value back to SECONDS register*/
-  time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_new_value);
+  time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_new_value);
   return OPERATION_DONE;
 }
 
 /*polls the ds1307 to see if its running*/
 uint8_t DS1307_run_state(void)
 {
-  time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+  time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
   if (register_current_value & (1 << DS1307_BIT_SETTING_CH))
     return DS1307_IS_RUNNING;
   else
@@ -99,59 +99,59 @@ void DS1307_reset(uint8_t option)
   switch (option)
   {
     case SECOND:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       HEX_to_BCD(&register_default_value[0], 1);
       register_new_value = (register_current_value & (1 << DS1307_BIT_SETTING_CH)) | register_default_value[0];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_new_value);
       break;
     case MINUTE:
       HEX_to_BCD(&register_default_value[1], 1);
       register_new_value = register_default_value[1];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, register_new_value);
       break;
     case HOUR:
       HEX_to_BCD(&register_default_value[2], 1);
       register_new_value = register_default_value[2] & (~(1 << DS1307_BIT_SETTING_AMPM));
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_HOURS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_HOURS, register_new_value);
       break;
     case DAY_OF_WEEK:
       HEX_to_BCD(&register_default_value[3], 1);
       register_new_value = register_default_value[3];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DAY_OF_WEEK, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DAY_OF_WEEK, register_new_value);
       break;
     case DATE:
       HEX_to_BCD(&register_default_value[4], 1);
       register_new_value = register_default_value[4];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DATE, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DATE, register_new_value);
       break;
     case MONTH:
       HEX_to_BCD(&register_default_value[5], 1);
       register_new_value = register_default_value[5];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MONTH, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MONTH, register_new_value);
       break;
     case YEAR:
       HEX_to_BCD(&register_default_value[6], 1);
       register_new_value = register_default_value[6];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_YEAR, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_YEAR, register_new_value);
       break;
     case CONTROL:
       register_new_value = DS1307_REGISTER_CONTROL_DEFAULT;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, register_new_value);
       break;
     case TIME:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       HEX_to_BCD(&register_default_value[0], 1);
       register_new_value = (register_current_value & (1 << DS1307_BIT_SETTING_CH)) | register_default_value[0];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_new_value);
       HEX_to_BCD(&register_default_value[2], 1);
       register_default_value[2] &= (~(1 << DS1307_BIT_SETTING_AMPM));
       time_i2c_write_multi(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &register_default_value[1], 6);
       break;
     case ALL:        /*everything is reset but the general purpose ram*/
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       HEX_to_BCD(&register_default_value[0], 1);
       register_new_value = (register_current_value & (1 << DS1307_BIT_SETTING_CH)) | register_default_value[0];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_new_value);
       HEX_to_BCD(&register_default_value[2], 1);
       register_default_value[2] &= (~(1 << DS1307_BIT_SETTING_AMPM));
       time_i2c_write_multi(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &register_default_value[1], 7);
@@ -160,7 +160,7 @@ void DS1307_reset(uint8_t option)
       for (uint8_t register_address = DS1307_RAM_START; register_address <= DS1307_RAM_END; register_address++)
       {
         register_new_value = DS1307_RAM_BLOCK_DEFAULT;
-        time_i2c_write_single(DS1307_I2C_ADDRESS, register_address, &register_new_value);
+        time_i2c_write_single(DS1307_I2C_ADDRESS, register_address, register_new_value);
       }
       break;
     default:
@@ -174,46 +174,46 @@ uint8_t DS1307_read(uint8_t option, uint8_t *data_array)
   switch (option)
   {
     case SECOND:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       *data_array = register_current_value & (~(1 << DS1307_BIT_SETTING_CH));
       BCD_to_HEX(data_array, 1);
       break;
     case MINUTE:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, register_current_value);
       *data_array = register_current_value;
       BCD_to_HEX(data_array, 1);
       break;
     case HOUR:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_HOURS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_HOURS, register_current_value);
       *data_array = register_current_value & (~(1 << DS1307_BIT_SETTING_AMPM));
       BCD_to_HEX(data_array, 1);
       break;
     case DAY_OF_WEEK:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DAY_OF_WEEK, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DAY_OF_WEEK, register_current_value);
       *data_array = register_current_value;
       BCD_to_HEX(data_array, 1);
       break;
     case DATE:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DATE, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DATE, register_current_value);
       *data_array = register_current_value;
       BCD_to_HEX(data_array, 1);
       break;
     case MONTH:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MONTH, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MONTH, register_current_value);
       *data_array = register_current_value;
       BCD_to_HEX(data_array, 1);
       break;
     case YEAR:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_YEAR, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_YEAR, register_current_value);
       *data_array = register_current_value;
       BCD_to_HEX(data_array, 1);
       break;
     case CONTROL:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, register_current_value);
       *data_array = register_current_value;
       break;
     case TIME:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       data_array[0] = register_current_value & (~(1 << DS1307_BIT_SETTING_CH));
       time_i2c_read_multi(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &data_array[1], 6);
       BCD_to_HEX(data_array, 7);
@@ -221,7 +221,7 @@ uint8_t DS1307_read(uint8_t option, uint8_t *data_array)
     case SNAPSHOT:
       /*high level function to load a previously saved snapshot from ds1307 RAM. If there is
          no saved snapshots, returns nothing. needs an array of 7 bytes as input to function*/
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SNAP0_VACANCY, &snap0_vacancy);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SNAP0_VACANCY, snap0_vacancy);
       if (snap0_vacancy == OCCUPIED)
       {
         time_i2c_read_multi(DS1307_I2C_ADDRESS, DS1307_SNAP0_ADDRESS, data_array, 7);
@@ -231,7 +231,7 @@ uint8_t DS1307_read(uint8_t option, uint8_t *data_array)
       else
         return OPERATION_FAILED;
     case ALL:
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       data_array[0] = register_current_value & (~(1 << DS1307_BIT_SETTING_CH));
       time_i2c_read_multi(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &data_array[1], 7);
       BCD_to_HEX(data_array, 7);
@@ -249,57 +249,57 @@ uint8_t DS1307_set(uint8_t option, uint8_t *data_array)
   {
     case SECOND:
       HEX_to_BCD(data_array, 1);
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       register_new_value = (register_current_value & (1 << DS1307_BIT_SETTING_CH)) | *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_new_value);
       break;
     case MINUTE:
       HEX_to_BCD(data_array, 1);
       register_new_value = *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, register_new_value);
       break;
     case HOUR:
       HEX_to_BCD(data_array, 1);
       register_new_value = (~(1 << DS1307_BIT_SETTING_AMPM)) & *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_HOURS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_HOURS, register_new_value);
       break;
     case DAY_OF_WEEK:
       HEX_to_BCD(data_array, 1);
       register_new_value = *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DAY_OF_WEEK, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DAY_OF_WEEK, register_new_value);
       break;
     case DATE:
       HEX_to_BCD(data_array, 1);
       register_new_value = *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DATE, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_DATE, register_new_value);
       break;
     case MONTH:
       HEX_to_BCD(data_array, 1);
       register_new_value = *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MONTH, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_MONTH,register_new_value);
       break;
     case YEAR:
       HEX_to_BCD(data_array, 1);
       register_new_value = *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_YEAR, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_YEAR, register_new_value);
       break;
     case CONTROL:
       register_new_value = *data_array;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, register_new_value);
       break;
     case TIME:
       HEX_to_BCD(data_array, 7);
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       register_new_value = (register_current_value & (1 << DS1307_BIT_SETTING_CH)) | data_array[0];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_new_value);
       data_array[2] &= (~(1 << DS1307_BIT_SETTING_AMPM));
       time_i2c_write_multi(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &data_array[1], 6);
       break;
     case ALL:
       HEX_to_BCD(data_array, 7);
-      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_current_value);
+      time_i2c_read_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_current_value);
       register_new_value = (register_current_value & (1 << DS1307_BIT_SETTING_CH)) | data_array[0];
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_SECONDS, register_new_value);
       data_array[2] &= (~(1 << DS1307_BIT_SETTING_AMPM));
       time_i2c_write_multi(DS1307_I2C_ADDRESS, DS1307_REGISTER_MINUTES, &data_array[1], 7);
       break;
@@ -318,11 +318,11 @@ uint8_t DS1307_square_wave(uint8_t input)
   {
     case WAVE_OFF:
       register_new_value = 0X00;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, register_new_value);
       return OPERATION_DONE;
     case WAVE_1:
       register_new_value = 0X10;
-      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, &register_new_value);
+      time_i2c_write_single(DS1307_I2C_ADDRESS, DS1307_REGISTER_CONTROL, register_new_value);
       return OPERATION_DONE;
     case WAVE_2:
       register_new_value = 0X11;

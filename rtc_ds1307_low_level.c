4aa0ca2fa93f04634e4197c7745a2eceed0fe6ae
holmiumdev1 <158459896+holmiumdev1@users.noreply.github.com>
2024-11-27 17:19:44 +0530

    Update rtc_ds1307_low_level.c
    esp-idf support

--- rtc_ds1307_low_level.c
+++ rtc_ds1307_low_level.c
@@ -1,27 +1,52 @@
 /*ds1307 low level api - Reza Ebrahimi v1.0*/
 #include "rtc_ds1307.h"
 
+#include "i2c.h"
+#include "string.h"
 /*function to transmit one byte of data to register_address on ds1307*/
 void time_i2c_write_single(uint8_t device_address, uint8_t register_address, uint8_t *data_byte)
 {
+    uint8_t array[2] = {0};
+    array[0] = register_address;
+    array[1] = &data_byte;
+    i2c_master_write_to_device(I2C_NUM_0, device_address,
+                                     &array, 2,
+                                     pdMS_TO_TICKS(1000));
 }
 
 /*function to transmit an array of data to device_address, starting from start_register_address*/
 void time_i2c_write_multi(uint8_t device_address, uint8_t start_register_address, uint8_t *data_array, uint8_t data_length)
 {
+    uint8_t array[data_length + 1] = {0};
+    array[0] = start_register_address;
+    memcpy(&array[1],data_array,data_length);
+    i2c_master_write_to_device(I2C_NUM_0, device_address,
+                                     data_array, data_length,
+                                     pdMS_TO_TICKS(1000));
 }
 
 /*function to read one byte of data from register_address on ds1307*/
 void time_i2c_read_single(uint8_t device_address, uint8_t register_address, uint8_t *data_byte)
 {
+    i2c_master_write_read_device(I2C_NUM_0, device_address,
+                                       &register_address, 1,
+                                       data_byte, 1,
+                                       pdMS_TO_TICKS(1000))
+                                      
 }
 
 /*function to read an array of data from device_address*/
 void time_i2c_read_multi(uint8_t device_address, uint8_t start_register_address, uint8_t *data_array, uint8_t data_length)
 {
+    i2c_master_write_read_device(I2C_NUM_0, device_address,
+                                       &start_register_address, 1,
+                                       data_array, data_length,
+                                       pdMS_TO_TICKS(1000))
 }
 
 /*function to initialize I2C peripheral in 100khz*/
 void DS1307_I2C_init()
 {
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+    i2c_master_driver_initialize();
 }

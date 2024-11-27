0e8450aa70087ee783de43592645fc4a795b29ea
holmiumdev1 <158459896+holmiumdev1@users.noreply.github.com>
2024-11-27 17:54:32 +0530

    Update rtc_ds1307_low_level.c
    corrected driver/

--- rtc_ds1307_low_level.c
+++ rtc_ds1307_low_level.c
@@ -1,7 +1,7 @@
 /*ds1307 low level api - Reza Ebrahimi v1.0*/
 #include "include/rtc_ds1307.h"
 #include "freertos/FreeRTOS.h"
-#include "i2c.h"
+#include "driver/i2c.h"
 #include "string.h"
 /*function to transmit one byte of data to register_address on ds1307*/
 void time_i2c_write_single(uint8_t device_address, uint8_t register_address, uint8_t *data_byte)

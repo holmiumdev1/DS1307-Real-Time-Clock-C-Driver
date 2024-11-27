3a88eac9869a0ba1b5a8d75bec91bb711a692fda
holmiumdev1 <158459896+holmiumdev1@users.noreply.github.com>
2024-11-27 18:06:21 +0530

    Update rtc_ds1307.h

--- include/rtc_ds1307.h
+++ include/rtc_ds1307.h
@@ -1,7 +1,10 @@
 /*ds1307 driver header file - Reza Ebrahimi v1.0*/
 #ifndef RTC_DS1307_H
 #define RTC_DS1307_H
-#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/i2c.h"
+#include "string.h"
+#include "stdint.h"
 enum options {SECOND, MINUTE, HOUR, DAY_OF_WEEK, DATE, MONTH, YEAR, CONTROL, RAM, TIME, SNAPSHOT, ALL};
 enum square_wave {WAVE_OFF, WAVE_1, WAVE_2, WAVE_3, WAVE_4};
 enum ds1307_registers {
